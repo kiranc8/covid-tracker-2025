02ca462de63807ba5c9ac663c03c0e31c04405d8
kiran-c8 <kiranc8298@gmail.com>
2025-03-02 01:35:45 +0530

    dashboard component changes

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,31 +1,56 @@
 import { useEffect } from "react";
-
 import { useAppDispatch, useAppSelector } from "../library/hooks/reduxHook";
-import { fetchCovidData } from "../redux/covidDataSlice";
+import { fetchCovidData, fetchCovidStatHistory } from "../redux/covidDataSlice";
 import Dropdown from "../components/Dropdown";
 import StatsSection from "../components/StatsSection";
-
-import "../styles/Dashboard.css"
+import ChartSection from "../components/ChartSection";
+import Map from "../components/Map";
+import "../styles/Dashboard.css";
+import "../styles/Map.css"
 
 function Dashboard() {
   const dispatch = useAppDispatch();
   const covidData = useAppSelector((state) => state.covid.covidStat);
   const selectedState = useAppSelector((state) => state.covid.selectedRegion);
 
-  const filteredData = covidData.regional.filter((item) => {
-    return item.loc === selectedState;
-  });
-
-  console.log(filteredData)
+  const filteredData = covidData.regional.filter((item) => item.loc === selectedState);
 
   useEffect(() => {
     dispatch(fetchCovidData());
-  }, []);
-  
+    dispatch(fetchCovidStatHistory());
+  }, [dispatch]);
+
   return (
     <div className="dashboard-container">
-      <Dropdown regions={covidData.regional} />
-      {filteredData[0]?.loc && <StatsSection filteredData={filteredData[0]} />}
+      <header className="dashboard-header">
+        <div className="header-content">
+          <div className="header-titles">
+            <h1>COVID-19 Tracker</h1>
+            <p className="subtitle">Real-time Statistics & Visualizations</p>
+          </div>
+          <div className="header-controls">
+            <Dropdown regions={covidData.regional} />
+          </div>
+        </div>
+      </header>
+
+      <main className="dashboard-content">
+        {filteredData[0]?.loc && (
+          <>
+            <section className="stats-section">
+              <StatsSection filteredData={filteredData[0]} />
+            </section>
+
+            <section className="visualization-section">
+              <ChartSection filteredData={filteredData[0]} />
+              <div className="map-container">
+                <h3 className="map-title">{selectedState} Case Distribution Map</h3>
+                <Map stats={filteredData[0]} />
+              </div>
+            </section>
+          </>
+        )}
+      </main>
     </div>
   );
 }
