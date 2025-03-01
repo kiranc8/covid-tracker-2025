2eb16eb00a5f4de05b074aac7b969bb34efb2e78
kiran-c8 <kiranc8298@gmail.com>
2025-03-01 16:09:46 +0530

    Redux setup and API call using Thunk.

---
+++ src/interfaces/interface.ts
@@ -0,0 +1,43 @@
+export interface CovidDataState {
+  selectedRegion: string;
+  loading: boolean;
+  covidStat: CovidStat;
+}
+
+export interface CovidStat {
+  summary: Summary;
+  unofficialSummary: UnofficialSummary[];
+  regional: Regional[];
+  lastRefreshed: string;
+  lastOriginUpdate: string;
+}
+
+export interface Summary {
+  total: number;
+  confirmedCasesIndian: number;
+  confirmedCasesForeign: number;
+  discharged: number;
+  deaths: number;
+  confirmedButLocationUndefined: number;
+}
+
+export interface UnofficialSummary {
+  source: string;
+  total: number;
+  recovered: number;
+  deaths: number;
+  active: number;
+}
+
+export interface Regional {
+  loc: string;
+  confirmedCasesIndian: number;
+  confirmedCasesForeign: number;
+  discharged: number;
+  deaths: number;
+  totalConfirmed: number;
+}
+
+export interface ErrorInterface {
+  message: string;
+}
---
+++ src/redux/covidDataSlice.ts
@@ -0,0 +1,59 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import { CovidDataState } from "../interfaces/interface";
+
+const initialState: CovidDataState = {
+  selectedRegion: "Kerala",
+  loading: false,
+  covidStat: {
+    summary: {
+      total: 0,
+      confirmedCasesIndian: 0,
+      confirmedCasesForeign: 0,
+      discharged: 0,
+      deaths: 0,
+      confirmedButLocationUndefined: 0,
+    },
+    unofficialSummary: [],
+    regional: [],
+    lastRefreshed: "",
+    lastOriginUpdate: "",
+  },
+};
+
+export const fetchCovidData = createAsyncThunk(
+  "covid/fetchCovidData",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get(
+        "https://api.rootnet.in/covid19-in/stats/latest"
+      );
+      return response.data.data;
+    } catch (error: unknown) {
+      if (axios.isAxiosError(error)) {
+        return thunkAPI.rejectWithValue(
+          error.response?.data?.message || "API error"
+        );
+      }
+      return thunkAPI.rejectWithValue("An unknown error occurred");
+    }
+  }
+);
+
+export const CovidDataSlice = createSlice({
+  name: "covid",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchCovidData.pending, (state) => {
+        state.loading = false;
+      })
+      .addCase(fetchCovidData.fulfilled, (state, action) => {
+        state.loading = false;
+        state.covidStat = action.payload;
+      });
+  },
+});
+
+export default CovidDataSlice.reducer;
